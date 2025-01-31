73fcbedd147ed95b577c1eb517b8262c5f413ffd
Brianoyaro <brianoyaro2000@yahoo.com>
2025-01-31 14:12:41 +0300

    added babel to support older browsers

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(?:js|mjs|cjs|jsx)$/,
+                exclude: path.resolve(__dirname, '../node_modules/'),
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+              }
         ],
     },
     plugins: [new webpack.SourceMapDevToolPlugin()],
--- 0x01-react_intro/task_5/dashboard/dist/bundle.js
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -9,6 +9,17 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "../src/index.js":
+/*!***********************!*\
+  !*** ../src/index.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"../node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.css */ \"../src/index.css\");\n\n\nconst body = jquery__WEBPACK_IMPORTED_MODULE_0___default()('body');\nbody.append(\"<h1 class='hello-world'>Hello React from Webpack</h1>\");\n\n//# sourceURL=webpack:///../src/index.js?");
+
+/***/ }),
+
 /***/ "../node_modules/css-loader/dist/cjs.js!../src/index.css":
 /*!***************************************************************!*\
   !*** ../node_modules/css-loader/dist/cjs.js!../src/index.css ***!
@@ -126,17 +137,6 @@ eval("\n\n/* istanbul ignore next  */\nfunction apply(styleElement, options, obj
 
 "use strict";
 eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElement) {\n  if (styleElement.styleSheet) {\n    styleElement.styleSheet.cssText = css;\n  } else {\n    while (styleElement.firstChild) {\n      styleElement.removeChild(styleElement.firstChild);\n    }\n    styleElement.appendChild(document.createTextNode(css));\n  }\n}\nmodule.exports = styleTagTransform;\n\n//# sourceURL=webpack:///../node_modules/style-loader/dist/runtime/styleTagTransform.js?");
-
-/***/ }),
-
-/***/ "../src/index.js":
-/*!***********************!*\
-  !*** ../src/index.js ***!
-  \***********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ \"../node_modules/jquery/dist/jquery.js\");\n/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.css */ \"../src/index.css\");\n\n\n\n\nconst body = jquery__WEBPACK_IMPORTED_MODULE_0___default()('body');\n(body).append(\"<h1 class='hello-world'>Hello React from Webpack</h1>\");\n\n//# sourceURL=webpack:///../src/index.js?");
 
 /***/ })
 
